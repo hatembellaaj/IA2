b469ae862a72a00bfe9c94066f9c6c2eb2bb1d78
= <=>
2023-05-24 06:36:33 +0100

    fix cors problem

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import subprocess
 
 from flask import Flask, jsonify, request
-
+from flask_cors import CORS
 app = Flask(__name__)
-
-
+CORS(app)
 
 def run_command():
-
     return subprocess.Popen('python IAServicePython.py', shell=True, stdout=subprocess.PIPE).stdout.read()
-
-
 
 @app.route('/')
 def command_server():
@@ -22,6 +18,7 @@ def with_parameters():
     name = request.args.get('name')
     age = int(request.args.get('age'))
     ch = 'python witharguments.py '+ name + ' ' + str(age)
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return subprocess.Popen(ch, shell=True, stdout=subprocess.PIPE).stdout.read()
     #return jsonify(message="My name is " + name + " and I am " + str(age) + " years old")
 
